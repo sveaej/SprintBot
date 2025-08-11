7a424e311955e7c4284d6eb8f160368f431d32e8
sveaej <147221418+sveaej@users.noreply.github.com>
2025-08-11 19:59:48 -0400

    Implements a basic slash command

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const {Client} = require("discord.js");
+const {Client, Events, SlashCommandBuilder} = require("discord.js");
 const {token} = require("./config.json");
 
 const client = new Client({intents: []});
+
+client.once(Events.ClientReady, c => {
+    console.log(`Logged in as ${c.user.username}`);
+
+    const ping = new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with pong!");
+    
+    client.application.commands.create(ping);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+    if (interaction.commandName === "ping") {
+        interaction.reply("Pong!");
+    }
+});
+
 client.login(token);
