f5d8c7d040c8c1a6aef175d18e3551d9e3f78bff
sveaej <147221418+sveaej@users.noreply.github.com>
2025-08-11 22:41:38 -0400

    Changed the way I do commands

---
+++ commands/quote.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("quote")
+        .setDescription("Gives you a quote from ZenQuote"),
+    async execute(interaction) {
+        await interaction.deferReply();
+        try {
+            const response = await axios.get('https://zenquotes.io/api/random');
+            console.log(response);
+            await interaction.editReply("Testing");
+        }
+        catch(error) {
+            console.log(error);
+            await interaction.editReply("Unable to retrieve quote. Please try again later.");
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-const {Client, Events, SlashCommandBuilder} = require("discord.js");
-const {token} = require("./config.json");
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Events, GatewayIntentBits, Collection} = require('discord.js');
+const {token} = require('./config.json');
 
 const client = new Client({intents: []});
 
-client.once(Events.ClientReady, c => {
-    console.log(`Logged in as ${c.user.username}`);
+client.commands = new Collection();
 
-    const ping = new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Checkin with the bot");
-    client.application.commands.create(ping);
+const commandsPath = path.join(__dirname, 'commands');
 
-    const quote = new SlashCommandBuilder()
-        .setName("quote")
-        .setDescription("Retrieve a random motivational quote to inspire you!");
-    client.application.commands.create(quote);
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    }
+    else {
+        console.log(`The command at ${filePath} is missing "data" and/or "execute" properties`);
+    }
+}
 
-    const prompt = new SlashCommandBuilder()
-        .setName("prompt")
-        .setDescription("Gives you a writing prompt from a writing prompts API.");
-    client.application.commands.create(quote);
-
-    const sprint = new SlashCommandBuilder()
-        .setName("sprint")
-        .setDescription("Starts a writing sprint and sets a timer for your desired length");
-    client.application.commands.create(sprint);
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found`);
+        return;
+    }
+    try {
+        await command.execute(interaction);
+    }
+    catch(error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command', ephemeral: true });
+        }
+        else {
+            await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true })
+        }
+    }
 });
 
-client.on(Events.InteractionCreate, interaction => {
-    if (interaction.commandName === "ping") {
-        interaction.reply("Pong!");
-    }
-    if (interaction.commandName === "quote") {
-        interaction.reply("Motivational quotes will be shared here!");
-    }
-    if (interaction.commandName === "prompt") {
-        interaction.reply("This command will eventually call a writing prompts API... but I probably have to make one first.");
-    }
-    if (interaction.commandName === "sprint") {
-        interaction.reply("This command will start a writing sprint. Ready or not, here we go!");
-    }
+
+client.once(Events.ClientReady, c => {
+    console.log(`Logged in as ${c.user.username}`);
 });
 
 client.login(token);
