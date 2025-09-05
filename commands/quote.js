4033c2e27235d90357f0c624e95cdd7bf11f683b
sveaej <147221418+sveaej@users.noreply.github.com>
2025-09-04 22:30:13 -0400

    Got quote working!! Responds as a string rn

--- commands/quote.js
+++ commands/quote.js
@@ -6,11 +6,12 @@ module.exports = {
         .setName("quote")
         .setDescription("Gives you a quote from ZenQuote"),
     async execute(interaction) {
-        await interaction.deferReply();
+        //await interaction.deferReply();
         try {
             const response = await axios.get('https://zenquotes.io/api/random');
-            console.log(response);
-            await interaction.editReply("Testing");
+            //console.log(response);
+            await interaction.reply(`${response.data[0].q} - ${response.data[0].a}`);
+            //await interaction.editReply("Testing");
         }
         catch(error) {
             console.log(error);
